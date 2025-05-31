4fe232399f59e1513a7bcb4f3624d267ebedb6b7
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-05-31 11:22:44 +0300

    développement du fonctionnalté d'iscription des utlisateurs

--- src/services/authentication.js
+++ src/services/authentication.js
@@ -1,7 +1,14 @@
-export function Register(request, response){
-    try {
+import { User } from "../models/User";
+import { connection, disconnection } from "./db";
 
+export async function Register(request, response){
+    try {
+        await connection()
+        // let user = await User.find({email:request})
+        console.log(request.body)
     }catch(err){
-        
+
+    }finally{
+        await disconnection()
     }
 }
