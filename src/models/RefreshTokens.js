3586154c8fc6c4931c15084ba246d040736dc191
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-06-09 08:39:03 +0300

    corréction de type de données dans le modèle

--- src/models/RefreshTokens.js
+++ src/models/RefreshTokens.js
@@ -3,7 +3,7 @@ import { model, Schema } from "mongoose";
 const RefreshTokensSchema = new Schema({
     user_id: { type: String, required: true },
     refresh_token: { type: String, required: true },
-    generation_date: { type: String, required: true, default: Date.now }
+    generation_date: { type: Date, required: true, default: Date.now }
 })
 
 export const RefreshToken = new model('RefreshTokens', RefreshTokensSchema)
