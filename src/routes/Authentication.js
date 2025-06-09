720474cb329485c54510e8d28b85825d9bf98398
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-06-09 06:10:18 +0300

    création de route de refresh token

--- src/routes/Authentication.js
+++ src/routes/Authentication.js
@@ -1,8 +1,10 @@
 import e from 'express'
-import { Login, Register } from '../services/authentication.js'
+import { Login, RefreshToken, Register } from '../services/authentication.js'
 
 export const auth_routes = e.Router()
 
 auth_routes.post('/register', Register)
 
 auth_routes.post('/login', Login)
+
+auth_routes.post('/token', RefreshToken)
