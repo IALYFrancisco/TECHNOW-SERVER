c5c7954c1d15cc6ec8b01d7a72b4f9b25ca64885
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-06-09 07:37:02 +0300

    création de route de déconnexion

--- src/routes/Authentication.js
+++ src/routes/Authentication.js
@@ -1,10 +1,12 @@
 import e from 'express'
-import { Login, _RefreshToken, Register } from '../services/authentication.js'
+import { Login, _RefreshToken, Register, Logout } from '../services/authentication.js'
 
 export const auth_routes = e.Router()
 
 auth_routes.post('/register', Register)
 
 auth_routes.post('/login', Login)
+
+auth_routes.post('logout', Logout)
 
 auth_routes.post('/token', _RefreshToken)
