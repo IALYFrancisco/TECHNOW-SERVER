8b85e6d29378ed0aeaae33157d39a67a8ff3839c
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-06-15 20:12:35 +0300

    suppression de route de vérification de token

--- src/routes/Authentication.js
+++ src/routes/Authentication.js
@@ -1,5 +1,5 @@
 import e from 'express'
-import { Login, _RefreshToken, Register, Logout, VerifyToken } from '../services/authentication.js'
+import { Login, _RefreshToken, Register, Logout } from '../services/authentication.js'
 
 export const auth_routes = e.Router()
 
@@ -9,6 +9,4 @@ auth_routes.post('/login', Login)
 
 auth_routes.post('/logout', Logout)
 
-auth_routes.post('/token/refresh', _RefreshToken)
-
-auth_routes.post('/token/verify', VerifyToken)
+auth_routes.post('/token', _RefreshToken)
