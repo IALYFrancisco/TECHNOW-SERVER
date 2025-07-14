0239d7afea0a4685b6eef85cb383f9f78c74b1de
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-07-14 15:21:25 +0300

    service de récupération de liste de produit développé

---
+++ src/services/product.js
@@ -0,0 +1,10 @@
+import { productsModel } from "../models/product.model"
+
+export async function GetProduct(request, response) {
+    try {
+        let product = await productsModel.find()
+        response.status(200).json(product)
+    }catch(err){
+        response.status(500).json({error: err})
+    }
+}
