1486d1e62974d1fbeba171b89f7bb6ed395ff9f8
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-07-14 15:45:37 +0300

    corréction du service de récupération de liste de produit

--- src/services/product.js
+++ src/services/product.js
@@ -1,8 +1,14 @@
+import { Product } from "../models/Product.js"
+import { connection, disconnection } from "./db.js"
+
 export async function GetProduct(request, response) {
     try {
+        await connection()
         let product = await Product.find()
         response.status(200).json(product)
     }catch(err){
         response.status(500).json({error: err})
+    }finally{
+        await disconnection()
     }
 }
