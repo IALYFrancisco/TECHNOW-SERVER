e5ef1fe473ec2a71ea14d79d96a64d9000afb36d
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-07-15 12:11:14 +0300

    suppréssion d'autres codes inutiles

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,19 +1,3 @@
-
-import { Product } from '../models/Product.js'
-import { disconnection } from '../services/db.js'
-
 export function root(request,response) {
     response.json("Welcome to TECHNOW server 🖐")
-}
-
-export async function addProduct(request, response){
-
-    await disconnection()
-
-    let productCollection = new Product(request.body)
-
-    await productCollection.save()
-
-    response.status(201).json("Product added successfuly ✅✅")
-
 }
--- src/routes/Product.js
+++ src/routes/Product.js
@@ -4,3 +4,5 @@ import { GetProduct } from '../services/product.js'
 export const product_router = e.Router()
 
 product_router.get('/get', GetProduct)
+
+product_router.post('/add', addProduct)
